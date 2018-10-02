dbd95e60a11b3592b8bcc487c8c6a8366791c3d9
102464 <pxesrv@outlook.com>
2018-10-02 18:11:18 +0800

    Add database deployment script

---
+++ storekit.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Accounts(username varchar(32),password varchar(32));
+CREATE TABLE AuthKey(username varchar(32),authkey varchar(10));
+CREATE TABLE Products(productname varchar(1024),productid varchar(10));
+CREATE TABLE Deposits(username varchar(32),deposit varchar(10));
+CREATE TABLE Payments(username varchar(32),productid varchar(10));
+INSERT INTO Accounts VALUES ('admin','admin');
