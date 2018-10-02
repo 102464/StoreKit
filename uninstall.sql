7bc12359dc6b57a2a510ef4be7067bfec39d2004
102464 <qwedcqwsxqaz@126.com>
2018-10-02 19:24:53 +0800

    Add a few descriptions

--- uninstall.sql
+++ uninstall.sql
@@ -1,2 +1,6 @@
 /* STOREKIT DATABASE UNINSTALLION SCRIPT */
+/* Open PHPStorm, connect to your database.
+Right-click the SQL command and click "run 'xxx.sql'"
+Then the uninstallion is successfully completed
+*/
 DROP DATABASE storekit;
