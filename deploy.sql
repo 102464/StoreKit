17d43a52d2f47012cadabf22905f0ed9bde84705
102464 <pxesrv@outlook.com>
2018-10-03 14:12:55 +0800

    Changed the type of 'deposit' in table 'Deposits'

--- deploy.sql
+++ deploy.sql
@@ -8,7 +8,7 @@ Then the deployment is successfully completed
 CREATE TABLE Accounts(username varchar(32),password varchar(32));
 CREATE TABLE AuthKey(username varchar(32),authkey varchar(10));
 CREATE TABLE Products(productname varchar(1024),productid varchar(10));
-CREATE TABLE Deposits(username varchar(32),deposit varchar(10));
+CREATE TABLE Deposits(username varchar(32),deposit decimal(16,2));
 CREATE TABLE Payments(username varchar(32),productid varchar(10));
 INSERT INTO Accounts VALUES ('admin','admin');
 /* DEFAULT Account and password: admin admin */                                                            
